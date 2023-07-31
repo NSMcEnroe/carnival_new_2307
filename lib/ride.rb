555838746272e2045eb3624bf1f24c2e3aa8aaf9
Nicholas McEnroe <nsmcenro@gmail.com>
2023-07-31 08:31:36 -0700

    Feat: Add initial ride class

---
+++ lib/ride.rb
@@ -0,0 +1,10 @@
+class Ride
+  attr_reader :name, :min_height, :admission_fee, :excitement, :rider_log
+  def initialize(ride_details)
+    @name = ride_details[:name]
+    @min_height = ride_details[:min_height]
+    @admission_fee = ride_details[:admission_fee]
+    @excitement = ride_details[:excitement]
+    @rider_log = {}
+  end
+end
---
+++ spec/ride_spec.rb
@@ -0,0 +1,20 @@
+require './lib/visitor'
+require './lib/ride'
+
+RSpec.describe Ride do
+  before(:each) do
+    @ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+  end
+
+  describe '#initialize' do
+    it "can initialize and have readable attributes" do
+      expect(@ride1).to be_a Ride
+      expect(@ride1.name).to eq("Carousel")
+      expect(@ride1.min_height).to eq(24)
+      expect(@ride1.admission_fee).to eq(1)
+      expect(@ride1.excitement).to eq(:gentle)
+      expect(@ride1.rider_log).to eq({})
+    end
+  end
+end
+
